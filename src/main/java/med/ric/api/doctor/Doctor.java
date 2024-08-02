476b1462998e97496b3fb866651f035df52dd5df
ricardo <ricardo85x@gmail.com>
2024-08-02 20:01:12 -0300

    feat: add Address and Doctor JPA

---
+++ src/main/java/med/ric/api/doctor/Doctor.java
@@ -0,0 +1,30 @@
+package med.ric.api.doctor;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.ric.api.address.Address;
+
+@Table(name = "doctors")
+@Entity(name = "Doctor")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Speciality speciality;
+
+    @Embedded
+    private Address address;
+}
