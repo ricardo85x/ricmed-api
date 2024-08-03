c26131b2909afd130153de778bcc539f7bb54d76
ricardo <ricardo85x@gmail.com>
2024-08-03 10:35:19 -0300

    feat: define default pagination value

--- src/main/java/med/ric/api/controller/DoctorController.java
+++ src/main/java/med/ric/api/controller/DoctorController.java
@@ -8,6 +8,7 @@ import med.ric.api.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,7 @@ public class DoctorController {
     }
 
     @GetMapping
-    public Page<DoctorListData> list(Pageable pagination) {
+    public Page<DoctorListData> list(@PageableDefault(size = 10, sort = { "name" }) Pageable pagination) {
         return repository.findAll(pagination).map(DoctorListData::new);
     }
 }
