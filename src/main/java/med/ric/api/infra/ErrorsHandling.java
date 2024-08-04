797f3f6cec3e1ed6c3284b1bbc285a61be3fdee3
ricardo <ricardo85x@gmail.com>
2024-08-04 14:42:38 -0300

    feat: handle method argument invalid errors

--- src/main/java/med/ric/api/infra/ErrorsHandling.java
+++ src/main/java/med/ric/api/infra/ErrorsHandling.java
@@ -2,8 +2,13 @@ package med.ric.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @RestControllerAdvice
 public class ErrorsHandling {
@@ -11,5 +16,17 @@ public class ErrorsHandling {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> handleException404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Stream<ValidationErrorData>> handleException400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorData::new));
+    }
+
+    public record ValidationErrorData(String field, String message) {
+        public ValidationErrorData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
