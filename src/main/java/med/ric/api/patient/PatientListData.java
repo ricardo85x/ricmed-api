57ae7ea9dbba2185303cfdb793e8fb45103d9c70
ricardo <ricardo85x@gmail.com>
2024-08-04 12:43:51 -0300

    feat: remove phone from exhibition from patient

--- src/main/java/med/ric/api/patient/PatientListData.java
+++ src/main/java/med/ric/api/patient/PatientListData.java
@@ -1,12 +1,11 @@
 package med.ric.api.patient;
 
-public record PatientListData(Long id, String name, String email, String phone, String cpf) {
+public record PatientListData(Long id, String name, String email, String cpf) {
     public PatientListData(Patient patient) {
         this(
             patient.getId(),
             patient.getName(),
             patient.getEmail(),
-            patient.getPhone(),
             patient.getCpf()
         );
     }
