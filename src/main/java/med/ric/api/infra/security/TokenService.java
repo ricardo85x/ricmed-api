f1a2f088694f9595b6bf88654005d9cd0bf9f270
ricardo <ricardo85x@gmail.com>
2024-08-05 15:25:44 -0300

    feat: create TokenService to handle token generation

---
+++ src/main/java/med/ric/api/infra/security/TokenService.java
@@ -0,0 +1,34 @@
+package med.ric.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.ric.api.domain.user.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public String generateToken(User user) {
+
+        try {
+            var algorithm = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API Ric.med")
+                    .withSubject(user.getLogin())
+                    .withClaim("id", user.getId())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error to generate token", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
