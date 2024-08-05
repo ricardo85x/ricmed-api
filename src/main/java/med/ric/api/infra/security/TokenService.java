61d49b5c20ea3d1cf53b456e0909648f85c18d12
ricardo <ricardo85x@gmail.com>
2024-08-05 15:58:16 -0300

    feat: update token service to read secret from env

--- src/main/java/med/ric/api/infra/security/TokenService.java
+++ src/main/java/med/ric/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import med.ric.api.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,10 +14,12 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    public String generateToken(User user) {
+    @Value("${api.security.token.secret}")
+    private String secret;
 
+    public String generateToken(User user) {
         try {
-            var algorithm = Algorithm.HMAC256("12345678");
+            var algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Ric.med")
                     .withSubject(user.getLogin())
