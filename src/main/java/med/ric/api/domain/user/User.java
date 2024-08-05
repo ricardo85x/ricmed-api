a788acb4b676d974793a40a54637c0a8dec861f1
ricardo <ricardo85x@gmail.com>
2024-08-05 10:42:06 -0300

    feat: create user entity and migration

---
+++ src/main/java/med/ric/api/domain/user/User.java
@@ -0,0 +1,22 @@
+package med.ric.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String login;
+    private String password;
+}
