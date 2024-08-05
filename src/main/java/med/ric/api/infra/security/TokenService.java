bf42b1d36e1c91358fb131305905d5d143bb42f1
ricardo <ricardo85x@gmail.com>
2024-08-05 17:42:27 -0300

    feat: added function to get the subject of jwt

--- src/main/java/med/ric/api/infra/security/TokenService.java
+++ src/main/java/med/ric/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.ric.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.ric.api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,20 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error to generate token", exception);
+        }
+    }
+
+
+    public String getSubject(String token) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Ric.med")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token invalid", exception);
         }
     }
 
