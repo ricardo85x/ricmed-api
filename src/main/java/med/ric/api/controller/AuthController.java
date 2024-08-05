283defbd1367415fcbd5c004919f32d7b2c790ba
ricardo <ricardo85x@gmail.com>
2024-08-05 12:58:39 -0300

    feat: wip setup login route
    and security defaults

---
+++ src/main/java/med/ric/api/controller/AuthController.java
@@ -0,0 +1,28 @@
+package med.ric.api.controller;
+
+import jakarta.validation.Valid;
+import med.ric.api.domain.user.AuthData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity doLogin(@RequestBody @Valid AuthData data) {
+        var token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var authentication = manager.authenticate(token);
+
+      return ResponseEntity.ok().build();
+   }
+}
--- src/main/java/med/ric/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/ric/api/infra/security/SecurityConfigurations.java
@@ -2,10 +2,14 @@ package med.ric.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +22,15 @@ public class SecurityConfigurations {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
